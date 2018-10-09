f9dd3fc75914774827b7ed0528aa69ce6e5947c4
Omar Orabi <oro1989@hotmail.com>
2018-10-09 08:48:17 +0200

    modified long urls to adhere to pep8

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Movie Trailer Website Project
 <ul>
 <li>-This Movie Trailer Website Project is part of Udacity's Full Stack Nanodegree program</li>
-<li>-You should have python installed to run this program</li>
+<li>-You should have python2.7 installed to run this program</li>
 <li>-start by running entertainment_center.py to create an HTML page of my favorite movies</li>
 <li>-Open the created fresh_tomatoes.html (you will find in the same directory)</li>
 <li>-Enjoy</li>
--- entertainment_center.py
+++ entertainment_center.py
@@ -3,27 +3,33 @@ import media
 
 # Creating multiple instances of the Movie class
 avatar = media.Movie("Avatar",
-                     "http://www.gstatic.com/tv/thumb/v22vodart/3542039/p3542039_v_v8_ac.jpg",
+                     "http://www.gstatic.com/tv/thumb/v22vodart/"
+                     "3542039/p3542039_v_v8_ac.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 matrix = media.Movie("Matrix",
-                     "http://www.gstatic.com/tv/thumb/v22vodart/22804/p22804_v_v8_as.jpg",
+                     "http://www.gstatic.com/tv/thumb/v22vodart/"
+                     "22804/p22804_v_v8_as.jpg",
                      "https://www.youtube.com/watch?v=m8e-FF8MsqU")
 
 forrest_gump = media.Movie("Forrest Gump",
-                           "http://www.gstatic.com/tv/thumb/v22vodart/15829/p15829_v_v8_ab.jpg",
+                           "http://www.gstatic.com/tv/thumb/v22vodart/"
+                           "15829/p15829_v_v8_ab.jpg",
                            "https://www.youtube.com/watch?v=bLvqoHBptjg")
 
 inception = media.Movie("Inception",
-                        "http://www.gstatic.com/tv/thumb/v22vodart/7825626/p7825626_v_v8_ad.jpg",
+                        "http://www.gstatic.com/tv/thumb/v22vodart/"
+                        "7825626/p7825626_v_v8_ad.jpg",
                         "https://www.youtube.com/watch?v=d3A3-zSOBT4")
 
 shutter_island = media.Movie("Shutter Island",
-                             "http://www.gstatic.com/tv/thumb/v22vodart/3531967/p3531967_v_v8_am.jpg",
+                             "http://www.gstatic.com/tv/thumb/v22vodart/"
+                             "3531967/p3531967_v_v8_am.jpg",
                              "https://www.youtube.com/watch?v=qdPw9x9h5CY")
 
 catch_me = media.Movie("Catch me if you can",
-                       "http://www.gstatic.com/tv/thumb/v22vodart/31064/p31064_v_v8_aa.jpg",
+                       "http://www.gstatic.com/tv/thumb/v22vodart/"
+                       "31064/p31064_v_v8_aa.jpg",
                        "https://www.youtube.com/watch?v=71rDQ7z4eFg")
 # create a list of movie instances
 movies = [avatar, matrix, forrest_gump, inception, shutter_island, catch_me]
